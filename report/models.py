fcd016b01af2569265581b60584b2ed04671039b
Vulpais <owen.lacroix@orange.fr>
2022-11-03 14:33:34 +0100

    ajout des models

--- report/models.py
+++ report/models.py
@@ -3,10 +3,22 @@ from django.db import models
 # Create your models here.
 
 class Report_data(models.Model):
-    id_bt = models.IntegerField()
-    pseudo_bt = models.CharField(max_length = 50)
-    id_game = models.IntegerField()
+    id_bt = models.ForeignKey(BetaTester)
+    id_bug = models.ForeignKey(Bug)
+    id_game = models.ForeignKey(Application)
+
+class BetaTester(models.Model):
+    nom = models.CharField(max_length = 30)
+    prenom = models.CharField(max_length = 30)
+    score = models.IntegerField()
+    score_bug = models.IntegerField()
+
+class Application(models.Model):
+    nom_app = models.CharField(max_length=30)
+
+class Bug(models.Model):
     bug_type = models.CharField(max_length = 50)
+
 
 # pour la classement visible que par les béta testeur
 # rajout d'une table béta testeur
